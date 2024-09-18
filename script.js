5071eed70fe254297f4d2315d79dd368c6d8ca7b
chrissundal <christoffersj@hotmail.com>
2024-09-18 11:41:51 +0200

    upd

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ let mailDetect = ''
 updateView();
 function updateView() {
     document.getElementById('app').innerHTML = /*HTML*/`
+    <div class="grid">
     <div class="main">
-        <div class="question">
-            <input type="text" onchange="getAnswer(this.value)" value="${question}">
-            <div>${mailDetect}</div>
-            <div>valid? ${showMessage}</div>
-            
+    <h1>Epost sjekker 2000</h1>
+            <div class="question">
+                <input type="text" onchange="getAnswer(this.value)" value="${question}">
+                <div class="detect">${mailDetect}</div>
+            </div>
+            <div>${showMessage}</div>
         </div>
     </div>
 `;
@@ -20,17 +22,30 @@ function updateView() {
 function getAnswer(text) {
     mailDetect = '';
     question = text;
-    showMessage = 'false';
+    showMessage = '';
     result = question.indexOf('@') + 1;
     if (question.includes('@')) {
         mailDetect = 'email'
         if (question.includes('.', `${result}`) && !question.includes(' ')) {
-            showMessage = 'true';
+            showMessage = `<button onclick="sendMail()">Send epost</button>`;
         } else {
-            showMessage = 'false'
+            showMessage = 'Må ha punktum, og ingen mellomrom'
         }
     } else {
-        showMessage = 'false'
+        showMessage = 'er ikke @'
     }
     updateView();
+}
+function sendMail() {
+    if (confirm('Vil du sende epost?') == true) {
+        alert('Beklager, har ikke funksjonalitet for dette enda')
+        mailDetect = '';
+        showMessage = '';
+        question = '';
+        updateView();
+    } else {
+        updateView();
+        alert('nei, så drit i det da')
+
+    }
 }
